ecbb8c6f6a50a0cb4b2293dc902669241a06f2ac
D0rs4n <41237606+D0rs4n@users.noreply.github.com>
2023-01-11 01:56:01 +0100

    Add Bounty Creation functionality.
    - Create a named tuple, for representing Bug Bounty programs on the chain
    - Create a `create_bounty_program` for adding a program to the chain.
    - Create a `set_authorized_members` function to rewrite who can accept bounties, inside a program.

--- contract.py
+++ contract.py
@@ -3,7 +3,11 @@ from typing import Final
 from beaker import Application, ApplicationStateValue, Authorize
 from beaker.decorators import (close_out, create, delete, external, opt_in,
                                update)
-from pyteal import Approve, Expr, Global, Reject, TealType, abi
+from beaker.lib.storage import Mapping
+from pyteal import (Approve, Assert, Expr, Global, Int, Reject, Seq, TealType,
+                    Txn, abi)
+
+from mappings import BountyProgram
 
 
 class ContractoriumPlatform(Application):
@@ -12,6 +16,11 @@ class ContractoriumPlatform(Application):
     manager: Final[ApplicationStateValue] = ApplicationStateValue(
         stack_type=TealType.bytes, default=Global.creator_address()
     )
+
+    bounty_programs = Mapping(abi.Address, BountyProgram)
+
+    MaxMembers = Int(1000)
+    MinimumBalance = Int(2500)
 
     @create
     def create(self) -> Expr:
@@ -46,3 +55,44 @@ class ContractoriumPlatform(Application):
         The transaction can only succeed if the invoker is the current manager.
         """
         return self.manager.set(new_manager.get())
+
+    @external
+    def create_bounty_program(
+            self,
+            name: abi.String,
+            authorized_members: abi.DynamicArray[abi.Address],
+            description: abi.String,
+    ) -> Expr:
+        """
+        A method to create and store a bug bounty program on the Algorand Blockchain, using Boxes.
+
+        It accepts a name, a list of authorized members, who can accept bounties,
+        and a description of the program itself.
+        """
+        return Seq(
+            (new_bounty_program := BountyProgram()).set(name, authorized_members, description),
+            self.bounty_programs[Txn.sender()].set(new_bounty_program),
+        )
+
+    @external
+    def set_authorized_members(
+            self,
+            new_authorized_members: abi.DynamicArray[abi.Address],
+            *,
+            output: BountyProgram
+    ) -> Expr:
+        """
+        A method to update the members who can accept bounties.
+
+        Note, that only the program creator can modify this.
+        """
+        tmp_name = abi.String()
+        tmp_description = abi.String()
+        return Seq(
+            Assert(self.bounty_programs[Txn.sender()].exists()),
+            self.bounty_programs[Txn.sender()].store_into(output),
+            (output.name.store_into(tmp_name)),
+            (output.description.store_into(tmp_description)),
+            (modified_bounty_program := BountyProgram()).set(tmp_name, new_authorized_members, tmp_description),
+            self.bounty_programs[Txn.sender()].set(modified_bounty_program),
+        )
